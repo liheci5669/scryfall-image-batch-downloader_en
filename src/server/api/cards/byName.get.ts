67c78e24f7be71c351ee75ca74a0dbbca308ba1c
Kintsuba <bnxth107@gmail.com>
2022-09-28 13:34:55 +0900

    feat: change to useFetch per card

--- src/server/api/cards/byName.get.ts
+++ src/server/api/cards/byName.get.ts
@@ -4,5 +4,15 @@ export default defineEventHandler(async (event) => {
   const query = useQuery(event);
   const name = query.name as string;
   const card = await Scry.Cards.byName(decodeURI(name));
-  return card;
+
+  try {
+    const jaCard = await Scry.Cards.bySet(
+      card.set,
+      parseInt(card.collector_number),
+      "ja"
+    );
+    return jaCard;
+  } catch {
+    return card;
+  }
 });
