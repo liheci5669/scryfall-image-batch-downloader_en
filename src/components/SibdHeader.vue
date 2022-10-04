bb32e638603cdc4299915d7d256dbafb8e66e9fa
Kintsuba <bnxth107@gmail.com>
2022-10-04 15:27:39 +0900

    feat: add link of repository to header

--- src/components/SibdHeader.vue
+++ src/components/SibdHeader.vue
@@ -1,15 +1,27 @@
 <template>
-  <header class="bg-primary-500 p-2 mt-0 w-full shadow">
-    <div class="container mx-auto flex flex-wrap items-center">
-      <div
-        class="flex w-full justify-center md:justify-start text-white font-extrabold"
-      >
+  <header class="bg-primary-500 px-2 mt-0 w-full shadow">
+    <div
+      class="container mx-auto flex flex-wrap justify-between items-center w-full"
+    >
+      <div class="flex items-center text-white font-extrabold">
         <nuxt-link to="/">
           <span class="text-2xl pl-2 py-2 font-display font-medium">
             Scryfall Image Batch Downloader
           </span>
         </nuxt-link>
       </div>
+      <div class="flex text-right text-white font-bold">
+        <nuxt-link
+          to="https://github.com/kintsuba/scryfall-image-batch-downloader"
+          class="p-2 flex items-center hover:bg-primary-700"
+        >
+          <IconGitHub color="currentcolor" width="28" height="28" />
+          <span class="pl-1 text-lg hidden md:inline"> Repository </span>
+        </nuxt-link>
+      </div>
     </div>
   </header>
 </template>
+<script setup lang="ts">
+import IconGitHub from "~icons/mdi/github";
+</script>
