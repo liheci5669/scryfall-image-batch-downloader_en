34729427c10d16804da13b509a2d2fe8cf88f91f
Kintsuba <bnxth107@gmail.com>
2022-10-06 17:14:37 +0900

    feat: change color of the loading spinner

--- src/components/ScryLoading.vue
+++ src/components/ScryLoading.vue
@@ -1,7 +1,7 @@
 <template>
   <div role="status">
     <svg
-      class="inline mr-2 w-10 h-10 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+      class="inline mr-2 w-10 h-10 text-gray-200 animate-spin dark:text-gray-600 fill-primary-600"
       viewBox="0 0 100 101"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
